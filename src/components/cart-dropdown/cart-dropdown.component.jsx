eddeaa359091ff2ef0d2d1112a034ba9aa49a5b6
Sean Kerwin <swkerwin@icloud.com>
2022-12-21 22:05:03 +0000

    fix unused var

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import CartItem from "../cart-item.component/cart-item.component";
